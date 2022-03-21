054f8ffab65db07dc1855ad2b6a1fe24fba98141
Turbosprezarka <99955817+Turbosprezarka@users.noreply.github.com>
2022-03-21 09:24:24 +0100

    Add files via upload
    Algorytm wyznaczania liczb pierwszych

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ cout << "Ile liczb pierwszych chcesz wyświetlić?: ";
+ unsigned int n, liczbap, liczbyt, dzielnik;
+ bool t;  
+ cin >> n;
+ liczbap = 0;
+ liczbyt = 2;
+ while (liczbap < n)
+ {
+    t = true;
+    for (dzielnik = 2; dzielnik < liczbyt; dzielnik++)
+        if (liczbyt % dzielnik == 0)
+        {
+            t = false;
+            break;
+            }
+    
+if (t)
+{
+cout << liczbyt << " ";
+liczbap++;
+}
+liczbyt++;
+ }
+cout << endl;
+return 0;
+}
