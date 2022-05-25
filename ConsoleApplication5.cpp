485e62e40a5a5a03dd8d21d2104c6700596b4da7
Turbosprezarka <99955817+Turbosprezarka@users.noreply.github.com>
2022-05-25 23:32:54 +0200

    Add files via upload

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+
+void kod(int n)
+{
+    long long a = 1, b = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cout << b << " ";
+        b += a; 
+        a = b - a; 
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Ile wyrazow ciagu wypisac: ";
+    cin >> n;
+    kod(n);
+    return 0;
+}
